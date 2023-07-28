3963182e50c3223104d459cf8b79c43e184d2efa
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-28 17:18:48 +0300

    Scanner

--- coreJava/src/coreJava/KeybRead.java
+++ coreJava/src/coreJava/KeybRead.java
@@ -6,7 +6,6 @@ import java.util.*;
 public class KeybRead {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner s = new Scanner(System.in);
 		int a,b,c;
 		String name;
