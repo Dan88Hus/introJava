c193231a7d9371f5c9a779d2aeb6380706bfb2fc
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-28 18:39:24 +0300

    Literals

--- coreJava/src/coreJava/Literals.java
+++ coreJava/src/coreJava/Literals.java
@@ -1,15 +1,26 @@
 package coreJava;
-"""
-		Literals are constant variables of a program
-		z = 5*x+7*y, 5 and 7 are literals
-		double quotes are literals and single quotes are char
-		
-		"""
+
+import java.util.Scanner;
+
+/*
+Literals are constant variables of a program
+z = 5*x+7*y, 5 and 7 are literals
+double quotes are literals and single quotes are char
+
+*/
+
 
 public class Literals {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String str;
+		Scanner z = new Scanner(System.in);
+		System.out.println("Please type String for z");
+		// z.nextLine();
+		str = z.nextLine();
+		System.out.println("here is why you typed: "+str);
+		
 
 	}
 
