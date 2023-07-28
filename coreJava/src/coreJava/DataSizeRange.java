3ac7038749ff7db16a7ce1e72de58dc670ea3530
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-28 17:35:51 +0300

    Data range

---
+++ coreJava/src/coreJava/DataSizeRange.java
@@ -0,0 +1,14 @@
+package coreJava;
+import java.lang.*;
+
+public class DataSizeRange {
+
+	public static void main(String[] args) {
+		int x;
+		Integer y;
+		System.out.println("Int min value: "+Integer.MIN_VALUE);
+		
+
+	}
+
+}
