f062793fb5e806d9ba58413dca5bf31b073ce417
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-29 15:24:37 +0300

    Inheritance

---
+++ coreJava/src/coreJava/Inheritance.java
@@ -0,0 +1,21 @@
+
+package coreJava;
+
+/**
+ * top to down, borrowing features from parent
+ * Parent ta olan methodlari extend ettigimiz icin parent ta 
+ * var olan method lari new liyebiliyoruz
+ * parent ta available olan methodlara erisebiliyoruz, accessible olanlar ise private and public
+ * 
+ */
+public class Inheritance extends ParentClass{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
