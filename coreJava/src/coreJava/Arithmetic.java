cc16b8b2c481f72e5fcf45adbe59b7dcf693e32e
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-29 14:44:25 +0300

    Arithmetic

---
+++ coreJava/src/coreJava/Arithmetic.java
@@ -0,0 +1,14 @@
+package coreJava;
+
+public class Arithmetic {
+
+	public static void main(String[] args) {
+		int a = 14, b = 5;
+		float c = (float)a/b;
+		int r = a%b;
+		System.out.println("remainder: "+r);
+		System.out.println("a/b: "+c);
+
+	}
+
+}
