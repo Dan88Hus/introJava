d237291fe77f9b34a98b277540201ce0f1596bc0
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-29 15:00:15 +0300

    OOP

---
+++ coreJava/src/coreJava/Oop.java
@@ -0,0 +1,19 @@
+package coreJava;
+
+/*
+ * abstract means we dont know what is inside
+ * encapsulation means capsuled, everthing is hided in box
+ * inheritance means add new features to old model product
+ * inheritance is specialicing in product
+ * polymorphism (generalization) tum marka tv ler televisions
+ * 
+ */
+public abstract class Oop {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
