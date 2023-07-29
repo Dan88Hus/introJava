97e70aacaec31a4ccfc8d9bd7b23fe3de3c8155e
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-29 16:09:40 +0300

    Interface

---
+++ coreJava/src/coreJava/Interfaces.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * Inheritance is borrowing
+ * abstract classin amaci polymorphisim veya inheritance
+ * interface, aims polymorpism only, not borrowing anything from super
+ * interface ler abstract classdir ondan abstract yazilmiyor method basina,
+ * implements kelimesi kullaniliyor, 1 den fazla implements yapabiliyoruz (polymorhisim)
+ * 
+ */
+
+public class Interfaces {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
