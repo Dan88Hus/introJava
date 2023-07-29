872de945aa2b2ff17c1572e1c9cba11b89fd39b1
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-29 16:00:08 +0300

    Abstract

---
+++ coreJava/src/coreJava/Abstract.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * Abstract class vs concrete class
+ * Abstract = classin onunde abstract yaziyor
+ * Concrete = normal classs lar cooncrete dir, onunde class yaziyor
+ * concretelerde method body var.
+ * can not be created object in abstract classes, new liyemiyoruz sadece referans verebiliyoruz Super sl;
+ * ama new Supel(); diyemiyoruz.
+ * concrete ler de new de yapabiiyoruz reference da verebiliyoruz
+ * abstract class larda method body si yok. (undefined method)
+ * 
+ * 
+ */
+public abstract class Abstract {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
