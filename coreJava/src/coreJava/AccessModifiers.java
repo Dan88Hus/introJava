824d82c771717719f8281be2f39971fbf3ffdf74
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-30 17:23:15 +0300

    AccessModifiers

---
+++ coreJava/src/coreJava/AccessModifiers.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * class can have variable, method and inner classes
+ * outer class can only be default or public
+ * 4 type of access modifiers,
+ * default = accessible within same package but not different package
+ * private
+ * protected = only not accessible in different packages, almost same as public 
+ * public
+ * 
+ */
+public class AccessModifiers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
