2af68f2c1a87bb22e69393bb6e9d2fb037922f7d
Huseyin Ozdogan <huseyinozdogan@gmail.com>
2023-07-31 16:50:12 +0300

    LambdaExpression

---
+++ coreJava/src/coreJava/MyLambda.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package coreJava;
+
+/**
+ * public void display() methodu 
+ * MyLambda m = ()-> {Sysout ("fddsfdsfsdf")}
+ * @FuntionalInterface demek call in icinde tek method var demektir.
+ * 
+ */
+public class MyLambda {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
